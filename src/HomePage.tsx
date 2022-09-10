af9e7f227f34a280d5b9168e7bc9fdfa5bd1c701
Kukuh Sulistyo <sulistyo.kukuh@gmail.com>
2022-09-11 05:57:30 +0700

    feat(HomePage): add skeleton UI

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Container, Grid, Heading } from "@chakra-ui/react";
+import { Container, Grid, Heading, Skeleton } from "@chakra-ui/react";
 
 import Header from "./components/Header/Header";
 import PokemonCard from "./components/PokemonCard";
 
 function HomePage() {
+  // FIXME: fetch data
+  const loading = true;
+
   return (
     <React.Fragment>
       <Header />
@@ -18,13 +21,17 @@ function HomePage() {
           gap={6}
           templateColumns={{ base: "repeat(2, 1fr)", sm: "repeat(3, 1fr)" }}
         >
-          {[...new Array(12)].map((_, id) => (
-            <PokemonCard
-              id={String(id + 1)}
-              name="bulbasuer"
-              types={["grass", "fire"]}
-            />
-          ))}
+          {[...new Array(12)].map((_, id) =>
+            loading ? (
+              <Skeleton borderRadius="xl" height="56" />
+            ) : (
+              <PokemonCard
+                id={String(id + 1)}
+                name="bulbasuer"
+                types={["grass", "fire"]}
+              />
+            )
+          )}
         </Grid>
       </Container>
     </React.Fragment>

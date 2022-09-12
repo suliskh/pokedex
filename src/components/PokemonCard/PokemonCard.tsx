f790b2131e94ca8bb2ce57ccea1db4abcc2a8d91
Kukuh Sulistyo <sulistyo.kukuh@gmail.com>
2022-09-13 06:53:34 +0700

    feat(PokemonCard): add support for selectable

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,24 +1,47 @@
-import { AspectRatio, Badge, Image, Link, Stack, Text } from "@chakra-ui/react";
+import { useRef } from "react";
 import { Link as RouterLink } from "react-router-dom";
+import {
+  AspectRatio,
+  Badge,
+  Checkbox,
+  Image,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 import { PokemonType } from "../../@types";
 import { getOfficialArtwork } from "../../utils";
 import PokemonFallbackImg from "../../assets/pokemon-fallback-img.png";
 
 type PokemonCardProps = Pick<PokemonType, "id" | "name"> & {
+  isSelectable?: boolean;
   types?: Array<string>;
   variant?: VariantType;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 type VariantType = "normal" | "compact";
 
 function PokemonCard({
   id,
+  isSelectable,
   name,
   types,
   variant = "normal",
+  onChange,
 }: PokemonCardProps) {
   const isNormalVariant = variant === "normal";
+
+  const checkboxRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (isSelectable) {
+      e.preventDefault();
+
+      checkboxRef.current?.click();
+    }
+  };
 
   return (
     <Link
@@ -26,13 +49,37 @@ function PokemonCard({
       backgroundColor="gray.50"
       borderRadius="xl"
       p={isNormalVariant ? "3" : "2"}
+      position="relative"
       to={"/pokemon/" + name}
       _focusVisible={{
         boxShadow: "outline",
         transform: "scale(105%)",
       }}
       _hover={{ textDecoration: "none", transform: "scale(105%)" }}
+      onClick={handleClick}
     >
+      {isSelectable && (
+        <Checkbox
+          aria-label={name}
+          m={isNormalVariant ? "3" : "2"}
+          position="absolute"
+          ref={checkboxRef}
+          right="0"
+          sx={{
+            ".chakra-checkbox__control:not([data-checked])": {
+              borderColor: "gray.400",
+            },
+          }}
+          top="0"
+          /**
+           * Combine `id` and `name` data separated by comma as the Checkbox only support
+           * string and number as value.
+           * Upon consumption, we need to destructure the `id` and `name` using `.split()`
+           */
+          value={[id, name].join(",")}
+          onChange={onChange}
+        />
+      )}
       <AspectRatio mb={isNormalVariant ? "3" : "1"} ratio={1}>
         <Image
           alt={name}
